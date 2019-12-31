b9747ce5a608c7424a3289ee613a765581afcc37
Mahe Duraiswami <maheshwari.duraiswami@gmail.com>
2019-12-31 18:00:20 -0500

    Microservices with Java Spring Boot and Spring Cloud

---
+++ currency-exchange-service/src/main/java/com/mahe/microservices/currencyexchangeservice/CurrencyExchangeServiceApplication.java
@@ -0,0 +1,26 @@
+package com.mahe.microservices.currencyexchangeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+
+import brave.sampler.Sampler;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class CurrencyExchangeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyExchangeServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler() {
+		return Sampler.ALWAYS_SAMPLE;
+		//return new AlwaysSampler();
+	}
+
+	
+}
